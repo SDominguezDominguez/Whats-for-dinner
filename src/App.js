814301e08a80d49327ccf98deb3c976ec1ba62b0
sadovaro <sdominguezdominguez@hotmail.com>
2022-08-18 19:37:36 +0200

    Routing geinstalleerd en geimplementeerd gemaakt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
+import SearchMyFridge from "./pages/SearchMyFridge/SearchMyFridge";
+import WeekMenu from "./pages/WeekMenu/WeekMenu";
+import WhatShouldIMake from "./pages/WhatShouldIMake/WhatShouldIMake";
 
 function App() {
   return (
-    <>
-
-    </>
-  );
+      <>
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="search-my-fridge" element={<SearchMyFridge/>}/>
+          <Route path="week-menu" element={<WeekMenu/>}/>
+          <Route path="what-should-i-make" element={<WhatShouldIMake/>}/>
+        </Routes>
+      </>
+  )
+      ;
 }
 
 export default App;
