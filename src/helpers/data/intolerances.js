c09c83ce01dcf1abedc1c01b62e453d21da78fe0
sadovaro <sdominguezdominguez@hotmail.com>
2022-08-21 21:38:08 +0200

    Made data objects for the different filter options and implemented them in the What should I make page

---
+++ src/helpers/data/intolerances.js
@@ -0,0 +1,16 @@
+const intolerances = [
+    "dairy",
+    "egg",
+    "gluten",
+    "grain",
+    "peanut",
+    "seafood",
+    "sesame",
+    "shellfish",
+    "soy",
+    "sulfite",
+    "tree Nut",
+    "wheat"
+]
+
+export default intolerances
